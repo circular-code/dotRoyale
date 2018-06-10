1db9a6f47a6294719fecb0be26d234077c9bda21
Stephanos Malikotsinas HOME PC <stephanos@malikotsinas.de>
2018-06-11 00:54:20 +0200

    added collision detection

--- app.js
+++ app.js
@@ -3,48 +3,23 @@
 /* jshint -W098 */
 
 var global = {
-  data: []
+  dots: []
 };
 
-function DataConstructor (speed, width, color) {
-  this.xPos = window.innerWidth / random(1,5);
+function Dot (speed, width, color) {
+  this.x = window.innerWidth / random(1,5);
   this.xDir = this.yDir = 1;
-  this.yPos = window.innerHeight / random(1,5);
+  this.y = window.innerHeight / random(1,5);
   this.speed = speed;
   this.width = width;
   this.radius = width / 2;
   this.color = color;
+  this.destroyed = false;
 }
 
-function setup() {
-  createCanvas(window.innerWidth, window.innerHeight);
-}
-
-setInterval(pushDataConstructor, 1000);
-
-function pushDataConstructor() {
-  global.data.push(new DataConstructor(random(5,15), random(10,50), 'rgba(' + floor(random(0,256)) + ',' + floor(random(0,256)) + ',' + floor(random(0,256)) + ',100)'));
-}
-
-function draw() {
-
-  background('#000000');
-
-  global.data.forEach(function(d) {
-    fill(d.color);
-    ellipse(determinePos(d.xPos, d.xDir, window.innerWidth, 'x', d), determinePos(d.yPos, d.yDir, window.innerHeight, 'y', d), d.width, d.width);
-  });
-}
-
-function rNumBetw (max, min) {
-  if (typeof max === 'undefined'){
-      console.log('max undefined');
-      return false;
-  }
-  if (typeof min === 'undefined'){
-      min = 0;
-  }
-  return Math.floor(Math.random()*(max-min)+min);
+function pushDots() {
+  if (global.dots.length < 20)
+    global.dots.push(new Dot(random(3,7), random(20,50), 'rgba(' + floor(random(0,256)) + ',' + floor(random(0,256)) + ',' + floor(random(0,256)) + ',100)'));
 }
 
 function determinePos (pos, direction, distance, axis, d) {
@@ -53,6 +28,64 @@ function determinePos (pos, direction, distance, axis, d) {
                     pos <= 0 + d.radius ? direction = 1 :
                     direction;
 
-  d[axis + 'Pos'] = pos += direction * d.speed;
+  d[axis] = pos += direction * d.speed;
   return pos;
 }
+
+function checkCollision(d) {
+  var collidedDots = global.dots.filter(function(dot) {
+    return d !== dot && dist(d.x, d.y, dot.x, dot.y) <= d.radius + dot.radius;
+  });
+
+  if (collidedDots.length > 0)
+    return collidedDots;
+}
+
+// function absorb(dots, mainDot) {
+
+//   dots.push(mainDot);
+
+//   var radius = 0;
+//   var index = 0;
+
+//   for (var i = 0; i < dots.length; i++) {
+//     if (dots[i].radius > radius) {
+//       radius = dots[i].radius;
+//       index = i;
+//     }
+//   }
+
+//   for (var k = 0; k < dots.length; k++) {
+//     if (dots.length[k] !== dots[index]) {
+//       dots[k].destroyed = true;
+//       global.dots[global.dots.indexOf(dots[k])].destroyed = true;
+//     }
+//   }
+// }
+
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+}
+
+function draw() {
+
+  background('#000000');
+
+  global.dots.forEach(function(d) {
+
+    var collidedDots = checkCollision(d);
+
+    if (collidedDots)
+      // absorb(collidedDots, d);
+      fill('red');
+    else
+      fill(d.color);
+
+    // if (d.destroyed)
+    //   return;
+
+    ellipse(determinePos(d.x, d.xDir, window.innerWidth, 'x', d), determinePos(d.y, d.yDir, window.innerHeight, 'y', d), d.width);
+  });
+}
+
+setInterval(pushDots, 1000);
